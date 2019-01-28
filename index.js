474786b2db227fb86df67349d6500647cd145f54
Joe Corwin <joe.corwin@gmail.com>
2019-01-28 01:22:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,9 +26,11 @@ function findBy(array, findFn) {
       return array[i]
     }
     return null
-
   }
 }
+
+
+
 
 
 // breakOut(array, changeValue, stopValue) which iterates through array and changes every element to changeValue until the loop reaches stopValue. Then we break out of the loop and return the array.
